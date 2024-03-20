540b2aeaddc9a3c865238420f188b55e6939cd69
ngochan-ngo <ngochan.ngo@student.adelaide.edu.au>
2024-03-20 17:01:36 +1030

    initial commit ws4

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 
-int array_sum(int array[], int n) {
-    if (n < 1) {
-        return 0;
+void printNumbers(int *numbers,int length) {
+    for (int i = 0; i < length; i++) {
+        std::cout << i << " " << numbers[i] << "\n";
     }
+}
 
-    int sum = 0;
-    
-    for (int i = 0; i < n; i++) {
-        sum += array[i];
-    }
-    return sum;
+int main() {
+    int array[] = {1,2,4,8};
+    printNumbers(array, 4);
 }
