3b813e467ae4de9e916704bdab1b1bb638154974
ngochan-ngo <161391723+ngochan-ngo@users.noreply.github.com>
2024-03-20 17:36:37 +1030

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+int* readNumbers() {
+    int *h = new int[10];
+
+    
+    for (int i = 0; i < 10; i++) {
+        std::cin >> h[i];
+    }
+
+    return h;
+}
 
 void printNumbers(int *numbers,int length) {
     for (int i = 0; i < length; i++) {
         std::cout << i << " " << numbers[i] << "\n";
     }
-}
-
-int main() {
-    int array[] = {1,2,4,8};
-    printNumbers(array, 4);
 }
