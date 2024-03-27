17804cba441430551a55c648c561987c44350628
ngochan-ngo <ngochan.ngo@student.adelaide.edu.au>
2024-03-27 23:06:47 +1030

    fifth

--- Musician.cpp
+++ Musician.cpp
@@ -2,17 +2,14 @@
 #include "Musician.h"
 
 int main() {
-Musician::Musician(); {
+Musician::Musician() {
     std::string instrument = "null";
     int experience = 0;
 }
 
-
 Musician::Musician(std::string instrument, int experience);
 
+std::string Musician::get_instrument() {return instrument;}
 
-
-std::string Musician::get_instrument();
-
-int Musician::get_experience();
+int Musician::get_experience() {return experience;}
 }
