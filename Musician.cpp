eab574539098bdd1af01fab9d92565308238a06f
ngochan-ngo <ngochan.ngo@student.adelaide.edu.au>
2024-03-27 23:11:26 +1030

    sixth

--- Musician.cpp
+++ Musician.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Musician.h"
 
+/*
 int main() {
 Musician::Musician() {
     std::string instrument = "null";
@@ -12,4 +13,25 @@ Musician::Musician(std::string instrument, int experience);
 std::string Musician::get_instrument() {return instrument;}
 
 int Musician::get_experience() {return experience;}
+}
+*/
+
+Musician::Musician() {
+  // Set default instrument and experience
+  instrument = "null";
+  experience = 0;
+}
+
+Musician::Musician(std::string instrument, int experience) {
+  // Set instrument and experience from arguments
+  instrument = instrument;
+  experience = experience;
+}
+
+std::string Musician::get_instrument() {
+  return instrument;
+}
+
+int Musician::get_experience() {
+  return experience;
 }
